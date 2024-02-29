b1c21eb2acf97d123952323dd0c0c713f27ce302
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-02-29 22:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
 
+let list = document.getElementById("infi-list");
+
+let itemNumber = 1;
+let addItems = function(){
+	for(let i = 0; i < 50; i++){
+		let item = document.createElement("li");
+		item.innerText = "item" + itemNumber++;
+		list.append(item);
+	}
+}
+
+list.addEventListener('scroll',function() {
+	if(list.scrollTop + list.scrollHeight >= list.scrollHeight){
+		// addItems();
+	}
+});
+
+addItems();
